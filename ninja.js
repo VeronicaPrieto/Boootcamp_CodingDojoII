c521144d407c8de3954ebd695cf63a49214fa5f3
Veronica Prieto <veroprieto2018@gmail.com>
2024-08-02 12:03:49 -0400

    initial commit

---
+++ ninja.js
@@ -0,0 +1,30 @@
+class Ninja {
+    constructor(nombre) {
+      this.nombre = nombre;
+      this.salud = 100;
+      this.velocidad = 3;
+      this.fuerza = 3;
+    }
+  
+    sayName() {
+      console.log(this.nombre);
+    }
+  
+    showStats() {
+      console.log(`Nombre: ${this.nombre}`);
+      console.log(`Fuerza: ${this.fuerza}`);
+      console.log(`Velocidad: ${this.velocidad}`);
+      console.log(`Salud: ${this.salud}`);
+    }
+  
+    drinkSake() {
+      this.salud += 10;
+    }
+  }
+  
+  // Ejemplo de uso
+  const ninja1 = new Ninja("Hyabusa");
+  ninja1.sayName();     
+  ninja1.showStats(); 
+  ninja1.drinkSake(); 
+  ninja1.showStats();  
