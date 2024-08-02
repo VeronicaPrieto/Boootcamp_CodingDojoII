1f0b42153e5cf62020de3c1ebfa3be83544958bf
Veronica Prieto <veroprieto2018@gmail.com>
2024-08-02 13:05:11 -0400

    [TAREA] sensei

--- Ninja/Sensei.js
+++ Ninja/Sensei.js
@@ -1,20 +1,23 @@
+// Importar la clase Ninja desde el archivo Ninja.js
+const Ninja = require('./ninja');
+
 // Clase Sensei que extiende de Ninja
-  class Sensei extends Ninja {
+class Sensei extends Ninja {
     constructor(nombre) {
-      super(nombre);
-      this.salud = 200;
-      this.velocidad = 10;
-      this.fuerza = 10; 
-      this.sabiduria = 10; 
+        super(nombre);
+        this.salud = 200;
+        this.velocidad = 10;
+        this.fuerza = 10;
+        this.sabiduria = 10;
     }
-  
+
     speakWisdom() {
-      this.drinkSake(); 
-      console.log("Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses.");
+        this.drinkSake();
+        console.log("Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses.");
     }
-  }
-  
-  // Ejemplo de uso
-  const superSensei = new Sensei("Master Splinter");
-  superSensei.speakWisdom();
-  superSensei.showStats();
+}
+
+// Ejemplo de uso
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+superSensei.showStats();
