731ec73396288e3532c3100651dbd1dd6bb6d15a
Veronica Prieto <veroprieto2018@gmail.com>
2024-08-02 13:05:26 -0400

    final commit

--- Ninja/ninja.js
+++ Ninja/ninja.js
@@ -1,30 +1,25 @@
 class Ninja {
-    constructor(nombre) {
+  constructor(nombre) {
       this.nombre = nombre;
       this.salud = 100;
       this.velocidad = 3;
       this.fuerza = 3;
-    }
-  
-    sayName() {
+  }
+
+  sayName() {
       console.log(this.nombre);
-    }
-  
-    showStats() {
+  }
+
+  showStats() {
       console.log(`Nombre: ${this.nombre}`);
       console.log(`Fuerza: ${this.fuerza}`);
       console.log(`Velocidad: ${this.velocidad}`);
       console.log(`Salud: ${this.salud}`);
-    }
-  
-    drinkSake() {
-      this.salud += 10;
-    }
   }
-  
-  // Ejemplo de uso
-  const ninja1 = new Ninja("Hyabusa");
-  ninja1.sayName();     
-  ninja1.showStats(); 
-  ninja1.drinkSake(); 
-  ninja1.showStats();  
+
+  drinkSake() {
+      this.salud += 10;
+  }
+}
+
+module.exports = Ninja; // Exportar la clase Ninja
